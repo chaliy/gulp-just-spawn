5e6819e982c4c74595a8563a64c69216748e6ec2
Mike Chaliy <mike@chaliy.name>
2016-05-27 17:02:04 +0300

    Added initial version

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let spawn = require('child_process').spawn;
+
+let cmd = (command, args, options) => {
+  if (Array.isArray(command)){    
+    let next = () => {
+      let c = command.shift();
+      if (c){
+        return cmd(c.cmd, c.args, c.options).then(next);
+      }
+      return Promise.resolve();
+    };
+    
+    return next();
+  }
+  
+  return new Promise((resolve, reject) => {
+    let procOptions = Object.assign({stdio: 'inherit', shell: true}, options || {});
+    let proc = spawn(command, args, procOptions);    
+
+    proc.on('exit', code => {      
+      console.log(code);
+      if (code === 0) resolve(code)
+      else reject(code)              
+    });
+  })
+}  
+
+
+module.exports = cmd;
